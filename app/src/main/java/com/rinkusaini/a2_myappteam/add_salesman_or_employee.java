8b170fc2839b5a7d79a26546caeb7b1069b0c3e4
narza05 <ankushsaini05112002@gmail.com>
2022-09-14 20:21:25 +0530

    add salesman class created

---
+++ app/src/main/java/com/rinkusaini/a2_myappteam/add_salesman_or_employee.java
@@ -0,0 +1,14 @@
+package com.rinkusaini.a2_myappteam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class add_salesman_or_employee extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_salesman_or_employee);
+    }
+}
